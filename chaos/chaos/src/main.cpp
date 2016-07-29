43bda989170f85ca3ffc0d4ede1a77ef9953a798
yoer <iamyoer@gmail.com>
2016-07-30 07:42:48 +0800

    load skin in start

--- chaos/chaos/src/main.cpp
+++ chaos/chaos/src/main.cpp
@@ -1,48 +1,38 @@
 ﻿#include <QApplication>
 #include <QtCore/qdebug.h>
 
-#include "core/qrframer.h"
 #include "db/qrframedb.h"
 #include "entity/qrsqldatabase.h"
 #include "qrrunguard.h"
 
+#include "qrchaosframer.h"
 #include "gui/qrchaosmainwindow.h"
 
-USING_NS_QRCOMMON;
-USING_NS_QRFRAME;
 USING_NS_CHAOS_BASE;
-
-#include <qfile.h>
 
 int main(int argc, char *argv[])
 {
-    QrRunGuard guard("qters.farme.chaos.v1");
+    Qters::QrCommon::QrRunGuard guard("qters.farme.chaos.v1");
     if ( !guard.tryToRun() ) {
         return 0;
     }
-
-    QApplication app(argc, argv);
 
     Qters::QrOrm::QrSqlDatabaseParams dbParam;
     dbParam.driverName = "QSQLITE";
     dbParam.folder = "data";
     dbParam.databaseName = "chaos.db";
 
-    QrFramerConfig frameConfig;
+    Qters::QrFrame::QrFramerConfig frameConfig;
     frameConfig.dbParams = dbParam;
     frameConfig.installLog = false;
 
-    QFile qssFile("blue.qss");
-    if(qssFile.open(QIODevice::ReadOnly))
-    {
-        QString styleSheet = QLatin1String(qssFile.readAll());
-        qDebug()<<styleSheet;
-        app.setStyleSheet(styleSheet);
-    }
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("Qters");
+    QCoreApplication::setApplicationName("chaos");
 
     QrChaosMainwindow mwindow;
-
-    QrFramer framer;
+    QrChaosFramer framer;
     framer.setConfig(frameConfig);
     framer.setMainWindow(&mwindow);
     framer.start();
